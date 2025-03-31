42a28d745a9891b20579db4d053bff64a95bbb7a
Тимофей Прохоренко <timoxa-pro030303@yandex.ru>
2025-03-31 15:32:42 +0300

    query params

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/blame/{name}")
-def read_root(name: str):
+@app.get("/blame?name=Nikita")
+def blame(name: str):
     return {"message": f"Hello {name}, YR loshara!"}
